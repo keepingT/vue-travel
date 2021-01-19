16d7b3b9343cb7a765cb4e367b6239648de9eeba
fs-tangjieping <tangjieping@richstonedt.com>
2021-01-19 12:01:18 +0800

    finish city css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="list">
+   <div class="item">A</div>
+   <div class="item">A</div>
+   <div class="item">A</div>
+   <div class="item">A</div>
+   <div class="item">A</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/variables.styl'
+  .list
+    display: flex
+    flex-direction: column
+    justify-content: center
+    position: absolute
+    top: 1.54rem
+    right: 0
+    bottom: 0
+    width: .4rem
+    .item
+      line-height: .4rem
+      text-align: center
+      color: $bgColor
+</style>
